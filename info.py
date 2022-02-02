5370045673d1a8f7d94bf2a3b234ef1edc30ec04
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-03 00:59:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/8b42f6caf6ef5fd76766f.jpg https://telegra.ph/file/82b5bbbab6d5e5593b6b2.jpg')).split()
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/0897cbe8a58e9f570379c.jpg https://telegra.ph/read-this-01-31-2 https://te.legra.ph/file/40fb481f9062166292f90.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
