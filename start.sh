cff9fb2ecb0cce3192595236ef43daf76f773d50
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-02 17:09:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
+git clone -b master https://github.com/Devansh20055/trial-v-5.git /trial-v-5
+cd /trial-v-5
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
