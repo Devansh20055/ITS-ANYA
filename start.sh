6f4a40ad5ac1e782883862b305d24fe3e3eb7232
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-03 15:17:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Devansh20055/trial-v-5.git /trial-v-5
-cd /trial-v-5
+git clone -b master https://github.com/Devansh20055/ITS-ANNYA.git /ITS-ANNYA
+cd /ITS-ANNYA
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
